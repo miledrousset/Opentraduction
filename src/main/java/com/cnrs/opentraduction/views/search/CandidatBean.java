a99ef769b598b984881f12e40f033f1b4dea8547
GABSI <firas.gabsi@gmail.com>
2024-07-21 16:20:40 +0200

    Finalisation de l'interface candidat

--- src/main/java/com/cnrs/opentraduction/views/search/CandidatBean.java
+++ src/main/java/com/cnrs/opentraduction/views/search/CandidatBean.java
@@ -1,5 +1,6 @@
 package com.cnrs.opentraduction.views.search;
 
+import com.cnrs.opentraduction.clients.DeeplService;
 import com.cnrs.opentraduction.entities.Users;
 import com.cnrs.opentraduction.models.client.CandidateModel;
 import com.cnrs.opentraduction.models.client.ElementModel;
@@ -31,18 +32,23 @@ public class CandidatBean implements Serializable {
 
     private final MessageService messageService;
     private final ThesaurusService thesaurusService;
+    private final DeeplService deeplService;
 
     private CandidatDao candidatDao;
     private Users userConnected;
     private List<CollectionElementDao> referenceCollectionList;
     private CollectionElementDao collectionReferenceSelected;
     private String labelReferenceCollectionSelected;
+    private boolean deeplDisponible;
 
 
     public void initInterface(Users userConnected) {
         log.info("Initialisation de l'interface candidat");
         this.userConnected = userConnected;
         candidatDao = new CandidatDao();
+
+        //Disable Deepl Translate function
+        deeplDisponible = false;
 
         var url = this.userConnected.getGroup().getReferenceInstances().getUrl();
         var idThesaurus = this.userConnected.getGroup().getReferenceInstances().getThesaurus().getIdThesaurus();
@@ -55,6 +61,7 @@ public class CandidatBean implements Serializable {
             var idCollection = this.userConnected.getGroup().getReferenceInstances().getThesaurus().getIdCollection();
             referenceCollectionList.addAll(thesaurusService.searchSubCollections(url, idThesaurus, idCollection));
         }
+        collectionReferenceSelected = referenceCollectionList.get(0);
     }
 
     public void saveCandidat() {
@@ -104,7 +111,7 @@ public class CandidatBean implements Serializable {
                 .terme(termes)
                 .definition(definitions)
                 .note(notes)
-                .collectionId(collectionReferenceSelected.getId())
+                .collectionId(getSelectedCollectionId())
                 .synonymes(synonymes)
                 .comment(candidatDao.getComment())
                 .build();
@@ -119,6 +126,15 @@ public class CandidatBean implements Serializable {
         messageService.showMessage(FacesMessage.SEVERITY_INFO, "application.candidat.success.msg1");
     }
 
+    private String getSelectedCollectionId() {
+        if ("ALL".equalsIgnoreCase(collectionReferenceSelected.getId())) {
+            return "ALL".equals(this.userConnected.getGroup().getReferenceInstances().getThesaurus().getIdCollection())
+                    ? "" : this.userConnected.getGroup().getReferenceInstances().getThesaurus().getIdCollection();
+        } else {
+            return collectionReferenceSelected.getId();
+        }
+    }
+
     public void setSelectedReferenceCollection() {
         if (!StringUtils.isEmpty(labelReferenceCollectionSelected)) {
             var collectionReferenceTmp = referenceCollectionList.stream()
@@ -126,6 +142,42 @@ public class CandidatBean implements Serializable {
                     .findFirst();
             collectionReferenceTmp.ifPresent(collectionElementDao -> collectionReferenceSelected = collectionElementDao);
         }
+    }
+
+    public void deeplTranslate(String value, String valueName) {
+
+        if (StringUtils.isEmpty(value)) {
+            messageService.showMessage(FacesMessage.SEVERITY_WARN, "application.candidat.error.emty.transition.value");
+            return;
+        }
+
+        switch (valueName) {
+            case "termFr":
+                candidatDao.setTitleAr(deeplService.translate(value, "fr", "ar"));
+                break;
+            case "termAr":
+                candidatDao.setTitleFr(deeplService.translate(value, "ar", "fr"));
+                break;
+            case "varianteFr":
+                candidatDao.setVarianteAr(deeplService.translate(value, "fr", "ar"));
+                break;
+            case "varianteAr":
+                candidatDao.setVarianteFr(deeplService.translate(value, "ar", "fr"));
+                break;
+            case "definitionFr":
+                candidatDao.setDefinitionAr(deeplService.translate(value, "fr", "ar"));
+                break;
+            case "definitionAr":
+                candidatDao.setDefinitionFr(deeplService.translate(value, "ar", "fr"));
+                break;
+            case "noteFr":
+                candidatDao.setNoteAr(deeplService.translate(value, "fr", "ar"));
+                break;
+            case "noteAr":
+                candidatDao.setNoteFr(deeplService.translate(value, "ar", "fr"));
+                break;
+        }
+        log.info("Traduction finish");
     }
 
 }
