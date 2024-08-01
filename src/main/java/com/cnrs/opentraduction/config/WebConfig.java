5fe43c61280e3faf01a5dd787e31dede9d8e43f4
GABSI <firas.gabsi@gmail.com>
2024-08-01 12:00:34 +0100

    Netoyage du projet

--- src/main/java/com/cnrs/opentraduction/config/WebConfig.java
+++ src/main/java/com/cnrs/opentraduction/config/WebConfig.java
@@ -15,10 +15,9 @@ public class WebConfig {
 
     @Bean
     public ServletRegistrationBean<FacesServlet> facesServletRegistration() {
-        ServletRegistrationBean<FacesServlet> registration = new ServletRegistrationBean<>(new FacesServlet(), "*.xhtml");
-        registration.setName("FacesServlet");
-        registration.setLoadOnStartup(1);
-        return registration;
+        ServletRegistrationBean<FacesServlet> registrationBean = new ServletRegistrationBean<>(new FacesServlet(), "*.xhtml");
+        registrationBean.setLoadOnStartup(1);
+        return registrationBean;
     }
 
     @Bean
