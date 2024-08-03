8aa2f045c267849612bf0c36902adb072d88b21f
GABSI <firas.gabsi@gmail.com>
2024-08-03 15:19:49 +0100

    Suppression de la redirection

--- src/main/java/com/cnrs/opentraduction/config/WebConfig.java
+++ src/main/java/com/cnrs/opentraduction/config/WebConfig.java
@@ -13,23 +13,15 @@ import javax.faces.webapp.FacesServlet;
 @Configuration
 public class WebConfig {
 
-    /*@Bean
+    @Bean
     public ServletRegistrationBean<FacesServlet> facesServletRegistration() {
         ServletRegistrationBean<FacesServlet> registrationBean = new ServletRegistrationBean<>(new FacesServlet(), "*.xhtml");
         registrationBean.setLoadOnStartup(1);
         return registrationBean;
-    }*/
-
-    @Bean
-    public ServletContextInitializer servletContextInitializer() {
-        return servletContext -> {
-            servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
-            servletContext.setInitParameter("primefaces.THEME", "nova-light");
-        };
     }
 
-    /*<@Bean
+    @Bean
     public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
         return new ServletListenerRegistrationBean<>(new ConfigureListener());
-    }*/
+    }
 }
